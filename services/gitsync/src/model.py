8ed43556c022df014599603d2f82f3a183481031
Paulito <paulito123.work@gmail.com>
2023-01-15 10:18:50 +0700

    WIP payment proposal

--- services/gitsync/src/model.py
+++ services/gitsync/src/model.py
@@ -70,7 +70,7 @@ class Contributor(Base):
                 )
                 if id:
                     c.id = id[0]
-                    session.merge()
+                    session.merge(c)
                 else:
                     session.add(c)
             session.commit()
@@ -78,27 +78,6 @@ class Contributor(Base):
         except Exception as e:
             print(f"[{datetime.now()}]:ERROR:{e}")
             return False
-
-
-class PaymentProposal(Base):
-    __tablename__ = "paymentproposal"
-
-    id = Column(Integer, primary_key=True)
-    # name of the discord account
-    discord_name = Column(String(64), nullable=True)
-    # link to on-chain identity
-    address_recipient = Column(String(32), nullable=True)
-    # links and descriptions to the issue
-    issue_details = Column(JSONB, nullable=True)
-    # amount of the bounty
-    amount = Column(BigInteger, nullable=True)
-    # memo regarding the payment
-    memo = Column(String(500), nullable=True)
-    # payment tx hash
-    tx_hash = Column(String(500), nullable=True)
-    # technical timestamps
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
 
 class GithubIssue(Base):
@@ -121,6 +100,9 @@ class GithubIssue(Base):
     assignees = Column(JSONB, nullable=True)
     labels = Column(JSONB, nullable=True)
     milestone = Column(String(200), nullable=True)
+    # unpaid, partial       --> unpaid issues that require attention
+    # paid, not required    --> paid issues that can be considered closed
+    payment_status = Column(String(50), nullable=False, default="unpaid")
     # technical dates
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
@@ -156,6 +138,135 @@ class GithubIssue(Base):
                 g.milestone = issue_gh.milestone
                 session.merge(g)
                 session.commit()
-            
+
+
+# 1. A new issue is closed
+# 2. The newly closed issue is crawled
+# 3. Payment proposal is created
+# 4. Proposals are sent to a (list of) designated person(s) by repo
+class PaymentProposal(Base):
+    __tablename__ = "paymentproposal"
+
+    id = Column(Integer, primary_key=True)
+    # payment tx hash
+    tx_hash = Column(String(500), nullable=True)
+    # name and/or id of the discord account
+    discord_details = Column(JSONB, nullable=False)
+    # link to on-chain identity
+    address_recipient = Column(String(32), nullable=True)
+    # links and descriptions to the issue
+    issue_details = Column(JSONB, nullable=False)
+    # amount of the bounty
+    amount = Column(BigInteger, nullable=True)
+    # memo regarding the payment
+    memo = Column(String(500), nullable=True)
+    # technical timestamps
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    def create_payment_proposal(issue_details: Dict) -> bool:
+        try:
+            ...
+            return True
+        except:
+            ...
+            return False
+
+
+    def get_update_metrics() -> Union[Tuple, None]:
+        return session.query(
+                func.max(PaymentProposal.updated_at),
+                func.count(PaymentProposal.id)
+            ).first()
+
+    def get_payment_proposals(self) -> Union[Dict, None]:
+        """ Generate a payment proposal json dict """
+        try:
+            metrics = self.get_update_metrics()
+            dict_out = {
+                "timestamp_last_update": datetime.strftime(metrics[0], Config.FORMAT_TIMESTAMP_TZ),
+                "data": []
+            }
+            payprop_list = session\
+                .query(
+                    PaymentProposal.tx_hash, 
+                    PaymentProposal.discord_details,
+                    PaymentProposal.address_recipient,
+                    PaymentProposal.issue_details,
+                    PaymentProposal.amount,
+                    PaymentProposal.memo)\
+                .all()
+            for payprop in payprop_list:
+                obj_out = {
+                    "tx_hash": payprop[0],
+                    "discord_details": payprop[1],
+                    "address_recipient": payprop[2],
+                    "issue_details": payprop[3],
+                    "amount": payprop[4],
+                    "memo": payprop[5]
+                }
+                dict_out['data'].append(obj_out)
+            return dict_out
+        except Exception as e:
+            print(f"[{datetime.now()}]:ERROR:{e}")
+            return None
+
+    def upload_payments_json(payment_proposals: Dict) -> bool:
+        """ Uploads a json file with payment proposals to the database """
+        try:
+            for payprop in payment_proposals['data']:
+                id = session\
+                    .query(PaymentProposal.id)\
+                    .where(PaymentProposal.issue_details.issue_id==payprop['issue_details']['issue_id'])\
+                    .first()
+                p = PaymentProposal(
+                    tx_hash = payprop['tx_hash'],
+                    discord_details = payprop['discord_details'],
+                    address_recipient = payprop['address_recipient'],
+                    issue_details = payprop['issue_details'],
+                    amount = int(payprop['amount']),
+                    memo = payprop['memo']
+                )
+                if id:
+                    p.id = id[0]
+                    session.merge(p)
+                else:
+                    session.add(p)
+            session.commit()
+            return True
+        except Exception as e:
+            print(f"[{datetime.now()}]:ERROR:{e}")
+            return False
+
 
 Base.metadata.create_all(engine)
+
+# List issues that have no existing payment proposals
+# /ls_is_open
+
+# List issues that have payment proposals but cannot be closed yet 
+# because more payment proposals must be generated in its context.
+# /ls_is_partial
+
+# List issues that either have no payment proposals because it is 
+# not required or all the proposals have been created in its context.
+# /ls_is_closed
+
+# List all proposals that have a txhash
+# /ls_prop_paid
+
+# List all proposals that have no txhash
+# /ls_prop_closed
+
+# show details of a specified object
+# /dtls_is <iid>
+# /dtls_prop <ppid>
+
+# Create a proposal for a specific issue
+# /create_prop <iid>
+
+# Edit the specified proposal
+# /edit_prop <ppid>
+
+# Close an issue (irreversable)
+# /close_is <iid>
