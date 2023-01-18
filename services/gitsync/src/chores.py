8261f276ab2d5d4cc3c36696ec5eafbe4cf9dd08
Paulito <paulito123.work@gmail.com>
2023-01-18 15:47:58 +0700

    Added safety checks to sync function

--- services/gitsync/src/chores.py
+++ services/gitsync/src/chores.py
@@ -11,34 +11,42 @@ def sync_contributors() -> None:
     try:
         # get file from github
         contributors = get_identities()
+
+        if not contributors:
+            raise Exception('Initiate contributors file!')
         
         file_last_ts = datetime.strptime(contributors['timestamp_last_update'], Config.FORMAT_TIMESTAMP_TZ)
         file_rec_count = len(contributors['data'])
-        
+
         # get last update date from db
         result = Contributor.get_update_metrics()
+
+        if not result:
+            raise Exception('Cannot connect to database or database model not initialized!')
         
-        db_last_ts = result[0]
+        if not result[0]:
+            db_last_ts = datetime.strptime("2000-01-01T00:00:00.000000+0000", Config.FORMAT_TIMESTAMP_TZ)
+        else:
+            db_last_ts = result[0]
         db_rec_count = result[1]
-        
-        if db_rec_count > 0:
+
+        if db_rec_count == 0 and file_rec_count == 0:
+            # Do nothing
+            ...
+        elif db_rec_count > 0:
             # compare timestamps
             if db_last_ts > file_last_ts and db_rec_count >= file_rec_count:
                 # If we arrive here, we assume the database has more recent data
-                # than the file on github. We therefore push the database data to
+                # than the file on github. We therefore push the database to
                 # the github file.
-                # update file
                 identities = Contributor.get_contributors(Contributor)
                 push_identities(identities)
-        else:
-            # If we arrive here, we assume the database is empty so we need to
-            # push the file data to the database.
-            # upload file to db
+        elif db_rec_count == 0 and file_rec_count > 0:
+            # if we arrive here, the db must be initialized
             if Contributor.upload_contributors_json(contributors):
                 # making sure the update timestamp is also pushed back to the file
                 identities = Contributor.get_contributors(Contributor)
                 push_identities(identities)
-
     except Exception as e:
         print(f"[{datetime.now()}]:ERROR:{e}")
 
